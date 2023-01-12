6348b06c525d1c01de1f55fdf62837c947c812c4
NapaNova <napanovatech@gmail.com>
2023-01-11 21:12:22 -0500

    initial commit

---
+++ popup.js
@@ -0,0 +1,5 @@
+document.getElementById('save-button').addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.executeScript(tabs[0].id, {file: "content.js"});
+    });
+});
